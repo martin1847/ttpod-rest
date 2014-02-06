e15428ad973e719864dc7076ac136edeebbf356d
CYY <272044275@qq.com>
2014-02-06 21:42:56 +0800

    ProtostuffRuntime

---
+++ ttpod-ttcp/src/main/java/com/ttpod/netty/protostuff/codec/ProtostuffRuntimeEncoder.java
@@ -0,0 +1,59 @@
+package com.ttpod.netty.protostuff.codec;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+
+/**
+ * date: 14-2-6 下午7:48
+ *
+ * @author: yangyang.cong@ttpod.com
+ */
+@ChannelHandler.Sharable
+public class ProtostuffRuntimeEncoder extends MessageToMessageEncoder<Object> {
+
+
+    /**
+     *
+     *http://code.google.com/p/protostuff/wiki/ProtostuffRuntime
+     *
+     * Performance guidelines
+     As much as possible, use the concrete type when declaring a field.
+
+     For polymorhic datasets, prefer abstract classes vs interfaces.
+
+     Use ExplicitIdStrategy to write the type metadata as int (ser/deser will be faster and the serialized size will be smaller).
+     Register your concrete classes at startup via ExplicitIdStrategy.Registry.
+     For objects not known ahead of time, use IncrementalIdStrategy
+     You can activate it using the system property:
+     -Dprotostuff.runtime.id_strategy_factory=com.dyuproject.protostuff.runtime.IncrementalIdStrategy$Factory
+     You can also use these strategies independently. E.g:
+     final IncrementalIdStrategy strategy = new IncrementalIdStrategy(....);
+     // use its registry if you want to pre-register classes.
+
+     // Then when your app needs a schema, use it.
+     RuntimeSchema.getSchema(clazz, strategy);
+
+     */
+
+    @Override
+    protected void encode(
+            ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
+
+//        ExplicitIdStrategy.Registry.
+        byte[] data = null;
+        Schema<Object>  schema = (Schema<Object>) RuntimeSchema.getSchema(msg.getClass());
+        data = ProtostuffIOUtil.toByteArray(msg, schema, LinkedBuffer.allocate(4096));
+
+//        schema.newMessage();
+        out.add(wrappedBuffer(data));
+    }
+}
---
+++ ttpod-ttcp/src/main/java/com/ttpod/netty/protostuff/main/ProtostuffClient.java
@@ -0,0 +1,50 @@
+package com.ttpod.netty.protostuff.main;
+
+import com.ttpod.netty.Client;
+import com.ttpod.netty.bean.codec.QueryReqDecoder;
+import com.ttpod.netty.bean.codec.QueryReqEncoder;
+import com.ttpod.netty.protostuff.Pojo;
+import com.ttpod.netty.protostuff.codec.ProtostuffRuntimeDecoder;
+import com.ttpod.netty.protostuff.codec.ProtostuffRuntimeEncoder;
+import io.netty.channel.*;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+import java.net.InetSocketAddress;
+
+/**
+ * date: 14-2-6 下午9:17
+ *
+ * @author: yangyang.cong@ttpod.com
+ */
+public class ProtostuffClient {
+    public static void main(String[] args) {
+        final QueryReqDecoder decoder =  new QueryReqDecoder();
+        final QueryReqEncoder encoder =  new QueryReqEncoder();
+
+//        byte flag = 1;
+////                                ctx.flush();
+//                                final ChannelFuture f = ctx.writeAndFlush(new QueryReq(flag,flag,flag,"周记录"));
+        new Client(new InetSocketAddress("127.0.0.1", 8080),
+                new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        p.addLast("frameDecoder", new ProtobufVarint32FrameDecoder());
+                        p.addLast("protobufDecoder", new ProtostuffRuntimeDecoder());
+
+                        p.addLast("frameEncoder", new ProtobufVarint32LengthFieldPrepender());
+                        p.addLast("protobufEncoder", new ProtostuffRuntimeEncoder());
+
+                        p.addLast("handler", new SimpleChannelInboundHandler<Pojo>() {
+                            @Override
+                            protected void messageReceived(ChannelHandlerContext ctx, Pojo msg) throws Exception {
+                                System.out.println(" ProtostuffClient Revice  :" + msg);
+                            }
+                        });
+                    }
+                });
+
+    }
+}
