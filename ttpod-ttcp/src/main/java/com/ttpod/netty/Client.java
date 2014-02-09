fb28a969fc3b7ea3e4becb4c1d412742cbd59e38
CYY <272044275@qq.com>
2014-02-09 20:48:14 +0800

    ResponseObserver refactory

--- ttpod-ttcp/src/main/java/com/ttpod/netty/Client.java
+++ ttpod-ttcp/src/main/java/com/ttpod/netty/Client.java
@@ -3,7 +3,9 @@ package com.ttpod.netty;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.socket.oio.OioSocketChannel;
 
 import java.net.SocketAddress;
 
@@ -16,35 +18,30 @@ public class Client {
     EventLoopGroup workerGroup;
 
     Channel channel;
+
     public Client(SocketAddress socketAddress, ChannelHandler channelHandler) throws InterruptedException {
-        workerGroup = new NioEventLoopGroup();
-            Bootstrap b = new Bootstrap(); // (1)
-            b.group(workerGroup); // (2)
-            b.channel(NioSocketChannel.class); // (3)
-            b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
-            b.handler(channelHandler);
-
-            // Start the client.
-            ChannelFuture f = b.connect(socketAddress).sync(); // (5)
-            // Wait until the connection is closed.
-            this.channel = f.channel();
-
-//                channel.closeFuture().sync();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-//            workerGroup.shutdownGracefully();
-//        }
+        this(socketAddress,true, channelHandler);
     }
 
+    public Client(SocketAddress socketAddress, boolean NIO, ChannelHandler channelHandler) throws InterruptedException {
+        this.workerGroup = NIO ? new NioEventLoopGroup() : new OioEventLoopGroup();
+        Bootstrap b = new Bootstrap(); // (1)
+        b.group(workerGroup); // (2)
+        b.channel( NIO ?NioSocketChannel.class : OioSocketChannel.class); // (3)
+        b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
+        b.handler(channelHandler);
+        // Start the client.
+        ChannelFuture f = b.connect(socketAddress).sync(); // (5)
+        // Wait until the connection is closed.
+        this.channel = f.channel();
+    }
 
     public void close() throws InterruptedException {
 //        channel.closeFuture().sync();
         channel.unsafe().closeForcibly();
-//        channel.pipeline(.fireExceptionCaught(ReadTimeoutException.INSTANCE);
-//        ctx.close();
         workerGroup.shutdownGracefully();
     }
+
     public Channel getChannel() {
         return channel;
     }
